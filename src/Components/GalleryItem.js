ddb11cc4329bc199a0fcb9d9feb69f1d3e544ba8
Laith Albusairi <laithalbusairi@gmail.com>
2022-10-18 20:45:37 -0700

    finshed hw

--- src/Components/GalleryItem.js
+++ src/Components/GalleryItem.js
@@ -1,16 +1,18 @@
+import { Link } from 'react-router-dom'
+
 const detailView = () => {
     return (
         <div style={detailStyle}>
             <h2>{props.item.trackName}</h2>
             <h3>
-                <a href={`/artist/${props.item.artistId}`}>
+                <Link to={`/artist/${props.item.artistId}`}>
                     {props.item.artistName}
-                </a>
+                </Link>
             </h3>
             <h3>
-                <a href={`/album/${props.item.collectionId}`}>
+                <Link to={`/album/${props.item.collectionId}`}>
                     {props.item.collectionName}
-                </a>
+                </Link>
             </h3>
             <h4>{props.item.primaryGenreName}</h4>
             <h4>{props.item.releaseDate}</h4>
